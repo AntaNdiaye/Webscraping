07600943c1dee825d07fba75a4b91b439e1ecd98
blell1 <blakeeller00@gmail.com>
2023-05-02 21:09:19 -0400

    Added ReadMe, small changes to csv file names, simplified SocrataDatasets.py

--- RunAll.py
+++ RunAll.py
@@ -2,11 +2,6 @@ with open("SocrataDatasests.py") as a:
     exec(a.read())
 with open("senior_project\senior_project\spiders\LAU_Data.py") as b:
     exec(b.read())
-with open("senior_project\senior_project\spiders\LAU_tables.py") as d:
-    exec(d.read())
 #ensure this file is last
 with open("senior_project\senior_project\spiders\LAU_Auto.py") as c:
     exec(c.read())
-
-
-    
